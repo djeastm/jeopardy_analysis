952526a63b5e1b6a96f49b1401aacc59d6b4df78
David Eastman <davidjeastman@gmail.com>
2017-06-01 16:46:08 -0400

    Initial commit

---
+++ scripts/process_html.py
@@ -0,0 +1,159 @@
+from bs4 import BeautifulSoup
+import re
+
+
+def main():
+    f = open('output.dat','w')
+    for num in range(1,11):
+        with open("data/showgame.php@game_id="+str(num)+".html",encoding='utf8') as fp:            
+            soup = BeautifulSoup(fp, 'lxml')
+            
+
+        # game metadata
+        game_id = str(num)
+        show_title = soup.find(id='game_title').string
+        show_num = re.search('\d+',show_title).group()
+
+        # categories
+        cat_tags = soup.find_all('td',class_='category_name')
+        categories = []
+        for tag in cat_tags:
+            cat_text = ''
+            if len(tag.contents) > 1:
+                cat_str = ''
+                for part in tag.strings:
+                    cat_str = cat_str + part                        
+                cat_text = str(cat_str)
+            else: 
+                cat_text = str(tag.string)
+            categories.append(cat_text)        
+
+##        # Set up final jeopardy
+##        fj = soup.find('table', class_='final_round')
+##        fj_category = fj.find(class_='category_name').string
+        
+        # Set up regular clues
+        clues = soup.find_all('td', class_='clue')
+        
+        i = 1;
+        for clue in clues:
+            fj = False;
+            blank = False;
+            clue_str = game_id+'|'+show_num+'|'
+##            print(clue_str+" "+str(i))
+            i = i+1
+
+            if clue.find(id='clue_FJ'):
+                fj = True                
+
+            ## Parse javascript for correct clue response            
+            if not fj:
+                # check for blank
+                if len(clue.contents)==1:
+                    blank = True;
+                else:
+                    js = clue.find('div')
+                    js_str = js['onmouseover']
+                    ## Get clue id, which includes round, category, and clue num
+                    ## e.g. clue_J_2_1                
+                    clue_id = re.search(r'clue_\w+_\d_\d', js_str).group().split('_')
+                    clue_round = str(clue_id[1])
+                    # get categories by indexing array, need to decrement cat num
+                    # and then add 6 to get double jeopardy
+                    # fj_category is at index 12
+                    if clue_round=='J':
+                        clue_cat = str(categories[int(clue_id[2])-1])
+                    elif clue_round=='DJ':
+                        clue_cat = str(categories[int(clue_id[2])+5])               
+                    else:
+                        clue_cat = 'n/a'
+                        
+                    clue_num = str(clue_id[3])
+    ##                print(clue_round+" "+clue_cat+" "+clue_num)                    
+                    clue_str = clue_str + clue_round+'|'+clue_cat+'|'+clue_num+'|'
+
+                    ## Get clue response from js
+                    ## (\w*|\s|\'|\.)+
+                    clue_response = re.search(r'onse">.*</em',js_str).group()[6:-4]
+                    # clean clue response of backslashes
+                    clue_response = clue_response.replace('\\','')
+                    # clean clue response of italics
+                    clue_response = clue_response.replace('<i>','')
+                    clue_response = clue_response.replace('</i>','')
+    ##                print(clue_response)
+            else:
+                # it's final jeopardy
+##                fj = True
+                clue_round = 'FJ'
+                clue_cat = categories[12]
+                clue_num = 'n/a'
+
+                clue_str = clue_str + clue_round+'|'+clue_cat+'|'+clue_num+'|'
+
+                # need to go to parent, then previous sibling, then find the div
+                par = clue.find_parent()
+                sib = par.find_previous_sibling()
+                div = sib.find('div')
+                js_str = div['onmouseover']
+                ## Get clue response from js
+                ## (\w*|\s|\'|\.)+
+                clue_response = re.search(r'onse\\?">.*</em',js_str).group()[7:-4]
+                # clean clue response of backslashes
+                
+                clue_response = clue_response.replace('\\','')
+                # clean clue response of italics
+                clue_response = clue_response.replace('<i>','')
+                clue_response = clue_response.replace('</i>','')                
+
+            clue_str = clue_str + clue_response+'|'
+            
+            ## Get clue text
+            clue_text = clue.find(class_='clue_text')                
+            # if there are more tags inside the clue text, just build
+            # the string manually, otherwise, convert the NavigableString
+            # to unicode
+
+            if not blank:
+                if len(clue_text.contents) > 1:
+                    text_str = ''
+                    for part in clue_text.strings:
+                        text_str = text_str + part                        
+                    clue_text = str(text_str)
+                else: 
+                    clue_text = str(clue_text.string)
+
+                clue_str = clue_str + clue_text +'|'           
+
+            
+                if not fj:
+                    ## get clue value for regular clues and placeholders for others
+                    clue_value = clue.find(class_='clue_value')
+                    if clue_value:
+                        clue_str = clue_str + clue_value.string + '|'
+                    elif clue.find(class_='clue_value_daily_double'):
+                        # it might be a daily double
+                        clue_str = clue_str + 'DD|'
+    ##                elif clue.find(id='clue_FJ'):
+    ##                    print('FJ')
+                    else:
+                        # it was never chosen
+                        clue_str = clue_str + 'NC|'
+
+                    ## get clue order number
+                    clue_order_num = clue.find(class_='clue_order_number')
+    ##                if clue_order_num:
+                    clue_str = clue_str + clue_order_num.string
+    ##                else:
+    ##                    print('FJ')                
+    ##            else:
+    ##                fj_str = fj_str + clue_text +',n/a,n/a'
+                    
+                # write clue string to file                
+                f.write(clue_str+'\n')
+        # write fj to file
+##        f.write(fj_str+'\n')
+        
+    f.close()
+    
+if __name__ == "__main__":
+    main()
